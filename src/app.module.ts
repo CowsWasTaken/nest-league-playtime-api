033ac65548506a6a09c66ac7093e3a329182e34f
Felix Jahn <74683084+CowsWasTaken@users.noreply.github.com>
2022-03-30 19:01:56 +0200

    Revert "Feature/prisma"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule, ConfigModule.forRoot()],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
