1ffe024049a5106a8c5cad523d4072b4abef2d18
Felix J <felix.hannes.fj@gmail.com>
2022-04-02 15:09:55 +0200

    feat: validation for req parameter setup

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MatchDto } from './matches/dto';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('test')
+  testing(@Body() dto: MatchDto): MatchDto {
+    return dto;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { MatchesModule } from './matches/matches.module';
 
 @Module({
-  imports: [PrismaModule, ConfigModule.forRoot()],
+  imports: [PrismaModule, ConfigModule.forRoot(), MatchesModule],
   controllers: [AppController],
   providers: [AppService],
 })
