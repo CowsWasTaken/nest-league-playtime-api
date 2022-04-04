b21912508e3979f7beb8c4ddc870ce574c151734
Felix J <felix.hannes.fj@gmail.com>
2022-04-04 17:48:24 +0200

    refactor: moved files and exported things from controller to service

--- src/league-api/dto/index.ts
+++ src/league-api/dto/index.ts
@@ -1,4 +1,4 @@
 export * from './match.dto';
 export * from './summoner.dto';
-export * from './MatchQueryParameter'
-export * from './MatchQueryType'
+export * from '../../models/MatchQueryParameter'
+export * from '../../models/MatchQueryType'
